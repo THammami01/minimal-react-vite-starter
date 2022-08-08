a7f75249e83e49cca79f4d41c022cf5356a398c9
Tarek Hammami <THammami.me@gmail.com>
2022-08-08 10:48:29 +0100

    chore: add .env and hide unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import "./App.scss";
+import _styles from "./App.module.scss";
 
 const App = () => {
-  return <div>Hello, World!</div>;
+  return <div>Hello, {import.meta.env.VITE_SECRET}!</div>;
 };
 
 export default App;
